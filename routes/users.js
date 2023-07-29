18713e7e137b153f04e0254de07b95cb74d6a8f4
Sahilnarang310 <softwaredev310@gamil.com>
2023-07-29 14:50:12 +0530

    Login backend api also encrypted the id in jwt

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router  = express.Router();
-const {signUp} = require('../controllers/users')
+const {signUp,login} = require('../controllers/users')
 
-
+router.route('/users/login').post(login)
 router.route('/users/signup').post(signUp);
 
 module.exports = router;
