395b4502848b29e28660d7e2fc00e46cb17e4ddf
Sahilnarang310 <softwaredev310@gamil.com>
2023-07-29 15:06:26 +0530

    Created Frontend and Backend of chatpage

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const {signUp,login} = require('../controllers/users')
 
-router.route('/users/login').post(login)
-router.route('/users/signup').post(signUp);
 
+router.route('/users/signup').post(signUp);
+router.route('/users/login').post(login)
 module.exports = router;
