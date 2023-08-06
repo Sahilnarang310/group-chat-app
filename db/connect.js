06a3cb85763565d347a42fa4e1081713a4ba602a
Sudipta jana <sudiptajana70@gmail.com>
2023-08-06 21:05:27 +0530

    refactor the code

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,14 @@
-const { Sequelize } = require ("sequelize");
-const mysql = require('mysql2')
+// Dependencies
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, { host: `localhost`, dialect: "mysql",logging:false });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
-
-
-
